27452880d7738dd6ba75b4bdd4ec789fae4a1ee3
Thomas Heyes <theyes@thoughtworks.com>
2016-11-23 23:43:08 +0530

    add new component for takng new answers and questions from the user

--- components/Answer.js
+++ components/Answer.js
@@ -2,47 +2,79 @@ import React, { Component, PropTypes } from 'react';
 import { View, StyleSheet, Text, TouchableHighlight } from 'react-native';
 
 import AnswerButtons from './AnswerButtons';
+import NewAnswerInput from './NewAnswerInput.js';
 
 export default class Answer extends Component {
-  static propTypes = {
-    answer: PropTypes.string.isRequired,
-    // onYes: PropTypes.func.isRequired,
-    // onNo: PropTypes.func.isRequired
-  }
+    static propTypes = {
+        answer: PropTypes.string.isRequired,
+        questionCount: PropTypes.number.isRequired
+        // onYes: PropTypes.func.isRequired,
+        // onNo: PropTypes.func.isRequired
+    }
 
-  constructor(props) {
-    super(props);
-    this.state = {text: props.answer};
-  }
+    constructor(props) {
+        super(props);
+        this.state = { gameOver: false };
+    }
 
-
-  render() {
-      return (
-          <View style={{flex: 1}}>
-            <View style={styles.answerTextView}>
-              <Text style={styles.answer} >
-                { this.state.text }
-              </Text>
-            </View>
-            <AnswerButtons
-              onYes = { () => {this.setState({text: "I win!" });}}
-              onNo = { () => {this.setState({text: "You win Congrats!" });}} >
-            </ AnswerButtons>
-          </View>
-      );
-  }
+    render() {
+        if(!this.state.gameOver) {
+            return (
+                <View style={{flex: 1}}>
+                  <View style={styles.answerTextView}>
+                    <Text style={styles.answer} >
+                      { this.props.answer }
+                    </Text>
+                  </View>
+                  <AnswerButtons
+                    onYes = { () => {this.setState({ playerWonGame: false,
+                    gameOver: true });}}
+                    onNo = { () => {this.setState({ playerWonGame: true,
+                    gameOver: true});}} >
+                  </AnswerButtons>
+                </View>
+            );
+        } else if(this.state.playerWonGame) {
+            return (
+                <NewAnswerInput
+                  newGame = { this.props.newGame }
+                  addNewQuestion = { this.props.addNewQuestion }
+                  wrongAnswer = { this.props.answer }
+                       >
+                </NewAnswerInput>
+            );
+        }
+        else if(!this.state.playerWonGame) {
+            return (
+                <View style={{flex: 1}}>
+                  <View style={styles.answerTextView}>
+                    <Text style={styles.answer} >
+                      Yay, I win!
+                    </Text>
+                    <Text style={styles.answer} >
+                      Shall we play again?
+                    </Text>
+                  </View>
+                  <AnswerButtons
+                    onYes = { this.props.newGame }
+                    onNo = { () => {this.setState({text: "Bye for now! :-)",
+                    playerWonGame: true});}} >
+                  </AnswerButtons>
+                </View> );
+        }
+    }
 }
 
 const styles = StyleSheet.create({
-  answerTextView:{
-    flex: 5,
-    backgroundColor: 'white',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  answer: {
-    margin: 10,
-    fontWeight: 'bold',
-    fontSize: 30
-  }
+    answerTextView:{
+        flex: 5,
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    answer: {
+        margin: 10,
+        fontWeight: 'bold',
+        fontSize: 30
+    }
 });
--- index.ios.js
+++ index.ios.js
@@ -1,9 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
     AppRegistry,
@@ -41,66 +35,79 @@ var questionTree = {
 export default class Pangolins extends Component {
 
     renderScene(route, navigator) {
-
         if(route.question.question){
-
             return ( <Question
-            question={ route.question }
-            questionCount={ route.questionCount }
+                     question={ route.question }
+                     questionCount={ route.questionCount }
 
-            onYes={ () => {
-              const nextIndex = route.questionCount + 1;
-              const nextQuestion = route.question.yes;
-                navigator.push({
-                  question: nextQuestion,
-                  questionCount: nextIndex
-              });
-            }}
+                     onYes={ () => {
+                         const nextIndex = route.questionCount + 1;
+                         const nextQuestion = route.question.yes;
+                         navigator.push({
+                             question: nextQuestion,
+                             questionCount: nextIndex
+                         });
+                     }}
 
-            onNo={() => {
-              const nextIndex = route.questionCount + 1;
-              const nextQuestion = route.question.no;
-                navigator.push({
-                  question: nextQuestion,
-                  questionCount: nextIndex
-              });
-            }}
-          />);
+                     onNo={() => {
+                         const nextIndex = route.questionCount + 1;
+                         const nextQuestion = route.question.no;
+                         navigator.push({
+                             question: nextQuestion,
+                             questionCount: nextIndex
+                         });
+                     }}
+                     />);
         } else{
-            return ( <Answer answer={route.question.answer} >
+            return ( <Answer
+                     answer={ route.question.answer }
+                     questionCount={ route.questionCount }
+                     newGame={() => {
+                         navigator.popToTop();
+                     }}
+                     addNewQuestion={(newAnswer, newQuestion, newQuestionIsYes) => {
+                         const yes =  { answer: newQuestionIsYes ? newAnswer : route.question.answer };
+                         const no = { answer: newQuestionIsYes ? route.question.answer : newAnswer };
+                         route.question = {
+                             question: newQuestion,
+                             yes: yes,
+                             no: no
+                         };
+                     }}
+                     >
                      </ Answer>);
         }
-  }
+    }
 
 
-  render() {
-    return (
-        <Navigator
-        style={{flex: 1}}
-        initialRoute={{ question: questionTree, questionCount: 0 }}
-        renderScene={ this.renderScene }
-      />
-    );
-  }
+    render() {
+        return (
+            <Navigator
+              style={{flex: 1}}
+              initialRoute={{ question: questionTree, questionCount: 0 }}
+              renderScene={ this.renderScene }
+              />
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
 
 AppRegistry.registerComponent('Pangolins', () => Pangolins);
